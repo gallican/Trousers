2b63bedde2bf7db3fb066f2dcfa643d6ac6f9fc8
uglybugger <andrewh@uglybugger.org>
2012-07-27 21:00:14 +1000

    Very first cut of AJAX UI

--- Trousers.Web/Controllers/HomeController.cs
+++ Trousers.Web/Controllers/HomeController.cs
@@ -4,5 +4,31 @@ namespace Trousers.Web.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Interactive");
+        }
+
+        public ActionResult Interactive()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(string expr)
+        {
+            if (expr.Contains("json")){
+            var result = new
+            {
+                isJson = true,
+                foo = 1,
+                bar = "two",
+            };
+
+            return Json(result);
+            }
+
+            return new ContentResult() { Content = "Hello, world!", ContentType="text/html" };
+        }
     }
 }
